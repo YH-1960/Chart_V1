29dd13bfccef3b6da9b6320df8414a41cedb967e
YH-1960 <papi6224@gmail.com>
2026-05-27 11:33:00 +0900

    Update app-6_2.py

--- app-6_2.py
+++ app-6_2.py
@@ -477,7 +477,7 @@ with left_col:
 
       # ボタン行
       # ★ 完全横並び（Streamlit columns）
-      btn1, btn2, btn3, spacer = st.columns([0.12, 0.12, 0.18, 1])
+      btn1, btn2, btn3, spacer = st.columns([0.12, 0.12, 0.22, 1])
 
       with btn1:
           up = st.button("↑", key=f"up_{i}")
